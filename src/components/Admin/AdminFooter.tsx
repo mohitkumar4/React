18516ac305a701800d995e915d1f94ae5d3658ad
Sri Mohit Kumar <kumar398593@gmail.com>
2024-11-30 17:24:30 +0530

    first commit

---
+++ src/components/Admin/AdminFooter.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AdminFooter = () => {
+
+    return (
+        <div className="d-flex bg-black justify-content-between px-5 pt-2">
+            <p><span>FriedShop | Admin Panel</span></p>
+            <p className="opacity-75">https://github.com/Pawan09Singh</p>
+        </div>
+    )
+}
+
+export default AdminFooter
